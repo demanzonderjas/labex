518e7b49b655affc91c69de7bdd7451d292115ce
Daan Weustenraad <daan.weustenraad@fxmedia.nl>
2020-06-15 08:18:44 +0200

    add factories to generate random data

--- database/factories/RequestFactory.php
+++ database/factories/RequestFactory.php
@@ -2,26 +2,28 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\ExchangeOffer;
+use App\ExchangeRequest;
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
-$factory->define(ExchangeOffer::class, function (Faker $faker) {
+function selectRandom($arr)
+{
+    $randomIndex = array_rand($arr);
+    return $arr[$randomIndex];
+}
+
+$factory->define(ExchangeRequest::class, function (Faker $faker) {
+
+
     return [
         "user_id" => User::first()->id,
         "type" => selectRandom(config("validation.type")),
         "animal_species" => selectRandom(config("validation.animal_species")),
         "organs" => selectRandom(config("validation.organs")),
-        "strain" => $faker->word(),
         "amount" => rand(0, 100),
         "sex" => selectRandom(config("validation.sex")),
-        "storage" => selectRandom(config("validation.storage")),
-        "age" => $faker->dateTimeThisYear(),
-        "date_available" => $faker->dateTimeThisMonth(),
         "origin" => selectRandom(config("validation.origin")),
-        "naive" => selectRandom(config("validation.yes_no")),
-        "protocol_number" => rand(10000, 99999),
         "spf" => selectRandom(config("validation.spf")),
-        "kill_method" => selectRandom(config("validation.kill_method"))
     ];
 });
