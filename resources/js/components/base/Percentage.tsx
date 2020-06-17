40e86022f47fe063808d2f0e42cff56a702d2f2e
Daan Weustenraad <daan.weustenraad@fxmedia.nl>
2020-06-17 12:57:03 +0200

    add match progress bar

--- resources/js/components/base/Percentage.tsx
+++ resources/js/components/base/Percentage.tsx
@@ -4,5 +4,12 @@ import { getMatchClasses } from "../../utils/formatting/matches";
 
 export function Percentage({ matchPercentage }) {
 	const classes = getMatchClasses(matchPercentage);
-	return <span className={cx("Percentage", classes)}>{matchPercentage | 0}%</span>;
+	return (
+		<span className={cx("Percentage", classes)}>
+			<span className="number">{matchPercentage | 0}%</span>
+			<span className="bar">
+				<span className="completed" style={{ width: `${matchPercentage}%` }} />
+			</span>
+		</span>
+	);
 }
--- resources/js/components/overviews/OverviewSwitch.tsx
+++ resources/js/components/overviews/OverviewSwitch.tsx
@@ -7,11 +7,11 @@ export const OverviewSwitch: React.FC = () => {
 	const { setOverviewType } = useSampleStore();
 	return (
 		<div className="OverviewSwitch">
-			<div className="image-wrapper" onClick={() => setOverviewType(OverviewType.Cards)}>
-				<LocalImage path="icons/cards-overview.svg" />
-			</div>
 			<div className="image-wrapper" onClick={() => setOverviewType(OverviewType.Table)}>
 				<LocalImage path="icons/table-overview.svg" />
+			</div>
+			<div className="image-wrapper" onClick={() => setOverviewType(OverviewType.Cards)}>
+				<LocalImage path="icons/cards-overview.svg" />
 			</div>
 		</div>
 	);
