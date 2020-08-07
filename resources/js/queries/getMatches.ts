6ceec599604c50defc9b707189e87fda8b35397c
Daan Weustenraad <daan.weustenraad@fxmedia.nl>
2020-08-07 09:09:09 +0200

    fix modal buttons

---
+++ resources/js/queries/getMatches.ts
@@ -0,0 +1,10 @@
+import { API } from "../utils/api/axios";
+
+export async function getMyMatches() {
+	try {
+		const response = await API.get("matches");
+		return response.data;
+	} catch (e) {
+		return { success: false, message: "invalid_request" };
+	}
+}
