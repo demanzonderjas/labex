b440cdd8c2b840d5e1fcfec7e841d255bfe9c970
Daan Weustenraad <daan.weustenraad@fxmedia.nl>
2020-10-19 17:14:17 +0200

    set default form homepage

--- resources/js/components/home/LoginForm.tsx
+++ resources/js/components/home/LoginForm.tsx
@@ -6,7 +6,7 @@ import { SignUpState } from "./SignupState";
 enum HomePageState { LOGIN, SIGN_UP };
 
 export const LoginForm: React.FC = () => {
-    const [page, setPage] = useState<HomePageState>(HomePageState.SIGN_UP);
+    const [page, setPage] = useState<HomePageState>(HomePageState.LOGIN);
 
     return (
         <div className="LoginForm layout-wrapper">
