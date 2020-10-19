cafe6c2faecde15ca2886e3f21196b2d7761fa8a
Daan Weustenraad <daan.weustenraad@fxmedia.nl>
2020-10-19 14:02:12 +0200

    add info steps

--- resources/js/pages/HomePage.tsx
+++ resources/js/pages/HomePage.tsx
@@ -21,7 +21,27 @@ export const HomePage: React.FC = () => {
                 </div>
             </div>
             <div className="column info">
-
+                <div className="info-steps layout-wrapper">
+                    <h3>{t("match_steps")}</h3>
+                    <p className="intro">{t("steps_intro")}</p>
+                    <div className="steps">
+                        <div className="step">
+                            <span className="number">1</span>
+                            <h4>{t("filter_existing")}</h4>
+                            <p>{t("filter_existing_description")}</p>
+                        </div>
+                        <div className="step">
+                            <span className="number">2</span>
+                            <h4>{t("select_match_or_save")}</h4>
+                            <p>{t("select_match_or_save_description")}</p>
+                        </div>
+                         <div className="step">
+                            <span className="number">3</span>
+                            <h4>{t("exchange_after_approval")}</h4>
+                            <p>{t("exchange_after_approval_description")}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
