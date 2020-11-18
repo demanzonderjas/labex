58e01edd48b88188cca0c0601711275c39ccf6d9
demanzonderjas <daan@puzzel.org>
2020-11-18 22:13:25 +0100

    sso

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,30 +7,19 @@ use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
-    /**
-     * Redirect the user to the GitHub authentication page.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function redirectToProvider()
     {
-        if (!Socialite::driver('surfconext')->user()) {
+        $user = Socialite::driver('surfconext')->user();
+        if ($user->accessTokenResponseBody) {
+            dd($user->accessTokenResponseBody);
+        } else {
             return Socialite::driver('surfconext')->redirect();
         }
-        $user = Socialite::driver('surfconext')->user();
-        dd("user", $user);
     }
 
-    /**
-     * Obtain the user information from GitHub.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function handleProviderCallback()
     {
         $user = Socialite::driver('surfconext')->user();
         dd("user", $user);
-
-        // $user->token;
     }
 }
