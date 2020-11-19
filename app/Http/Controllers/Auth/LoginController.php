620446a0942db5056a6845ccc9bc127527bb9789
demanzonderjas <daan@puzzel.org>
2020-11-19 09:50:15 +0100

    add signup flow

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Signup;
+use App\User;
+use Exception;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
@@ -14,7 +19,31 @@ class LoginController extends Controller
 
     public function handleProviderCallback()
     {
-        $user = Socialite::driver('surfconext')->user();
-        dd("user", $user);
+        try {
+            $ssoUser = Socialite::driver('surfconext')->user();
+            $existingUser = User::where('email', $ssoUser->email)->first();
+            if ($existingUser !== null) {
+                Auth::login($existingUser);
+                return redirect()->to('/app/dashboard');
+            } else {
+                $u = $this->createNewUser($ssoUser);
+                Auth::login($u);
+                return redirect()->to('/app/dashboard');
+            }
+        } catch (Exception $e) {
+            abort(403, $e->getMessage());
+        }
+    }
+
+    public function createNewUser($ssoUser): User
+    {
+        $u = new User();
+        $u->email = $ssoUser->email;
+        $u->name =  $ssoUser->name;
+        $u->token = Str::random(40);
+        $u->organisation = $ssoUser->schac_home_organization;
+        $u->save();
+
+        return $u;
     }
 }
