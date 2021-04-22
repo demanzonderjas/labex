f9d1de8442b27072bd9bc201f4f9b444a9a3dc34
Daan Weustenraad <weustenraad@gmail.com>
2021-04-22 13:39:42 +0200

    add article 9 recognition

--- database/migrations/2020_10_19_125706_create_signups_table.php
+++ database/migrations/2020_10_19_125706_create_signups_table.php
@@ -17,6 +17,7 @@ class CreateSignupsTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('article_9')->default(false);
             $table->boolean('awaiting_approval')->default(true);
             $table->boolean('approved')->default(false);
             $table->timestamps();
