6bc279f919ca8be798276bca79dce28f0b5bc0cd
Daan Weustenraad <weustenraad@gmail.com>
2021-12-14 13:39:08 +0100

    add explanation

--- resources/js/pages/AddNewAlert.tsx
+++ resources/js/pages/AddNewAlert.tsx
@@ -3,12 +3,16 @@ import { useHistory } from "react-router";
 import { FormWrapper } from "../components/FormWrapper";
 import { PageIntro } from "../components/layout/PageIntro";
 import { AddNewAlertForm } from "../data/forms/Alert";
+import { useTranslationStore } from "../hooks/useTranslationStore";
 
 export const AddNewAlertPage: React.FC = () => {
 	const { push } = useHistory();
+	const { t } = useTranslationStore();
 	return (
 		<div className="AddNewAlert">
-			<PageIntro header="add_new_alert" />
+			<PageIntro header="add_new_alert">
+				<div dangerouslySetInnerHTML={{ __html: t("add_new_alert_description") }} />
+			</PageIntro>
 			<div className="layout-wrapper" style={{ margin: "20px auto" }}>
 				<FormWrapper form={AddNewAlertForm} handleSuccess={() => push("/app/alerts")} />
 			</div>
