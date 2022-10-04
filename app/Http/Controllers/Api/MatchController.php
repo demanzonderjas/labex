da93ae6cf73ccf893e4c386b116de534491d77a4
Daan Weustenraad <weustenraad@gmail.com>
2022-10-04 09:37:40 +0200

    add reverse matches

--- app/Http/Controllers/Api/MatchController.php
+++ app/Http/Controllers/Api/MatchController.php
@@ -19,7 +19,7 @@ class MatchController extends Controller
     public function getAll()
     {
         $matches = MaterialMatch::with('offer.user', 'request.user')->get();
-        return response()->json(["success" => true, "matches" => $matches->toArray()]);
+        return response()->json(["success" => true, "matches" => $matches->reverse()->toArray()]);
     }
 
     public function user(Request $request)
