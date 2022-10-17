92195c0c9b49d6d03f0fc0a840b9e2ab5079dad4
Daan Weustenraad <weustenraad@gmail.com>
2022-10-17 13:39:37 +0200

    add stats

--- resources/js/typings/Stats.ts
+++ resources/js/typings/Stats.ts
@@ -2,5 +2,8 @@ export type TDashboardStats = {
 	requests: number;
 	offers: number;
 	matches: number;
+	users: number;
+	total_offered: number;
+	total_requested: number;
 	total_saved: number;
 };
