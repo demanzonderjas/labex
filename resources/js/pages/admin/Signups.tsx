250f0f165aa7e6f01c8c894e9ff0e3860f5d02f3
Daan Weustenraad <weustenraad@gmail.com>
2023-02-15 14:01:44 +0100

    fix filter

--- resources/js/pages/admin/Signups.tsx
+++ resources/js/pages/admin/Signups.tsx
@@ -30,7 +30,11 @@ export const SignupsPage = observer(() => {
 
 	const signupsWithCells = mapSignupsToOverviewData(
 		signups
-			.filter(s => s.name.match(filter.toLowerCase()) || s.email.match(filter.toLowerCase()))
+			.filter(
+				s =>
+					s.name.toLowerCase().match(filter.toLowerCase()) ||
+					s.email.toLowerCase().match(filter.toLowerCase())
+			)
 			.sort((a, b) => {
 				if (a[sortingKey] < b[sortingKey]) {
 					return reverse ? 1 : -1;
