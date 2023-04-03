56f3352c9570152c0b9759e7500d7c859092620d
Daan Weustenraad <weustenraad@gmail.com>
2023-04-03 11:24:11 +0200

    lowercase stats

---
+++ resources/js/typings/stats.ts
@@ -0,0 +1,10 @@
+export type TDashboardStats = {
+	requests: number;
+	offers: number;
+	matches: number;
+	users: number;
+	total_offered: number;
+	total_requested: number;
+	total_adopted: number;
+	total_saved: number;
+};
